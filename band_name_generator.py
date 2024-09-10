1b0eee090d5d4de664fd212705f2da016d75bdd6
Carmen <aisofiacatarino@gmail.com>
2024-09-11 00:32:28 +0100

    Initial commit code file and empty README file

---
+++ band_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+favourite_food_name = input("What's the name of your favourite food?\n")
+favourite_drink_name = input("What's your favourite drink?\n")
+print("Your band name could be " + favourite_food_name + " " + favourite_drink_name)
